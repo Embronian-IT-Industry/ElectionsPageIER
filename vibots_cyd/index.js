e5fe467a0220c1b7e0658381f0a9f3f5e620f0fb
Sepera_okeq <sepera173@gmail.com>
2020-06-08 03:12:09 +0400

    fix

--- vibots_cyd/index.js
+++ vibots_cyd/index.js
@@ -1,26 +1,28 @@
 am4core.useTheme(am4themes_animated);
 
-var chart = am4core.createFromConfig({
-  "data": [{
-    cydia: "Самогонщик",
-    litres: 25
-  },
+var chart = am4core.createFromConfig(
   {
-    cydia: "Сидорович",
-    litres: 22
-  },
-  {
-    cydia: "Симонов",
-    litres: 29
-  },
-  {
-    cydia: "Дейрон",
-    litres: 18
-  },
-  {
-    cydia: "Дорога",
-    litres: 14
-  }/*,
+    data: [
+      {
+        cydia: "Самогонщик",
+        litres: 25,
+      },
+      {
+        cydia: "Сидорович",
+        litres: 22,
+      },
+      {
+        cydia: "Симонов",
+        litres: 29,
+      },
+      {
+        cydia: "Дейрон",
+        litres: 18,
+      },
+      {
+        cydia: "Дорога",
+        litres: 14,
+      } /*,
   {
     cydia: "Хаус",
     litres: 15
@@ -38,14 +40,20 @@ var chart = am4core.createFromConfig({
     cydia: "The Netherlands",
     litres: 50
   
-  }*/ ],
-  "legend": {},
-  "innerRadius": "40%",
-  "series": [{
-    "type": "PieSeries3D",
-    "dataFields": {
-      "value": "litres",
-      "category": "cydia"
-    }
-  }]
-}, "chartdiv", "PieChart3D");
+  }*/,
+    ],
+    legend: {},
+    innerRadius: "40%",
+    series: [
+      {
+        type: "PieSeries3D",
+        dataFields: {
+          value: "litres",
+          category: "cydia",
+        },
+      },
+    ],
+  },
+  "chartdiv",
+  "PieChart3D"
+);
